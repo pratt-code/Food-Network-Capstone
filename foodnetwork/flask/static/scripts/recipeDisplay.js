d2ca7a096252362498d8e0e906b56fdf2f42d45b
pratt-code <michaelpratt1234@gmail.com>
2023-10-29 08:13:41 -0400

    Added filtering and home button

--- foodnetwork/flask/static/scripts/recipeDisplay.js
+++ foodnetwork/flask/static/scripts/recipeDisplay.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-    let debounce;
     let hidden = document.querySelectorAll(".recipe_hidden");
     let drop = document.querySelectorAll(".hide_button");
     for (i = 0; i<hidden.length; i++){
@@ -10,18 +9,64 @@ $(document).ready(function() {
             console.log(e.currentTarget.textContent)
             let icon = e.currentTarget.getElementsByTagName("i");
             if (icon[0].textContent == "expand_less"){
-                let d = e.currentTarget.id.substring(e.currentTarget.id.length-1, e.currentTarget.id.length);
+                let d = e.currentTarget.id.substring(4);
+                console.log(d)
                 document.getElementById(`hidden${d}`).style.display = "none";
                 icon[0].textContent = "expand_more";
             }
             else{
                 icon[0].textContent = "expand_less";
-                let d = e.currentTarget.id.substring(e.currentTarget.id.length-1, e.currentTarget.id.length);
+                let d = e.currentTarget.id.substring(4);
+                console.log(d)
                 console.log(document.getElementById(`hidden${d}`).display)
                 document.getElementById(`hidden${d}`).style.display = "inline-block";
             }
 
         })
+    }
+
+    /* Change color of calories container based on number */
+    let c = document.querySelectorAll(".calories");
+    for (i=0; i<c.length; i++){
+        let num = c[i].querySelector(".c_num");
+        
+        /* Set value of recipes without calories */
+        if (num.textContent == "No"){
+            num.textContent = "N/A";
+            c[i].style.backgroundColor = "#c2c2d6";
+        }
+        else if (parseInt(num.textContent) > 1000){
+            c[i].style.backgroundColor = "#ff6666";
+
+        }
+        else if (parseInt(num.textContent) > 650){
+            c[i].style.backgroundColor = "#ffff99";
+        }
+    }
+
+    /* Change "No" for nutrition info to N/A */
+    let n = document.querySelectorAll(".protein");
+    for (i=0; i<n.length; i++){ 
+        let p = n[i].querySelector("b");
+        if (p.textContent == "No g"){
+            p.textContent = "N/A"
+        }
+    }
+
+    let m = document.querySelectorAll(".fat");
+    for (i=0; i<m.length; i++){ 
+        let p = m[i].querySelector("b");
+        if (p.textContent == "No g"){
+            p.textContent = "N/A"
+        }
+    }
+
+    let o = document.querySelectorAll(".sodium");
+    for (i=0; i<o.length; i++){ 
+        let p = o[i].querySelector("b");
+        if (p.textContent == "No mg"){
+            p.textContent = "N/A"
+        }
     }
         
 })
